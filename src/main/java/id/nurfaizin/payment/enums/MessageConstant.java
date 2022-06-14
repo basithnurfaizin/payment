758b2c477458404e8aa9849c8e6872fc8c95a431
Basith Nurfaizin <basith@bts.id>
2022-06-14 21:52:50 +0700

    add exception when account is exist

---
+++ src/main/java/id/nurfaizin/payment/enums/MessageConstant.java
@@ -0,0 +1,27 @@
+package id.nurfaizin.payment.enums;
+
+public enum MessageConstant {
+
+    VIRTUAL_ACCOUNT_EXIST("false","13","Data Customer Sudah Ada");
+    private final String status;
+    private final String message;
+    private final String errorMessage;
+
+    MessageConstant(String status, String message, String errorMessage) {
+        this.status = status;
+        this.message = message;
+        this.errorMessage = errorMessage;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
